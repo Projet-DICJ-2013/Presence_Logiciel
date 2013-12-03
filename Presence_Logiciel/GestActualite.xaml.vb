03c03bd3936a5ba6838c52c9b09507d9e170658f
etb12345 <bolduc_etienne@hotmail.com>
2013-12-03 10:35:23 -0500

    actuaites

--- Presence_Logiciel/GestActualite.xaml.vb
+++ Presence_Logiciel/GestActualite.xaml.vb
@@ -3,10 +3,9 @@ Public Class rssActualite
     Public DM As PresenceEntities
     Dim LesActu As IQueryable(Of tblActualite)
     Dim nouvelleActualite As tblActualite
+    Dim vu As ListCollectionView
 
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        Me.Close()
-    End Sub
+
 
     Private Sub btnAdd_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles btnAdd.MouseDown
         nouvelleActualite = New tblActualite With _
@@ -30,6 +29,24 @@ Public Class rssActualite
 
         LesActu = (From actu In DM.tblActualite Select actu)
 
-        lstNouvelles.ItemsSource = CType(LesActu.First, tblActualite).TitreActu
+        lstNouvelles.ItemsSource = LesActu '(LesActu.First, tblActualite).TitreActu
+    End Sub
+
+    Private Sub lstNouvelles_MouseDoubleClick(sender As Object, e As MouseButtonEventArgs) Handles lstNouvelles.MouseDoubleClick
+        lstNouvelles.Visibility = Windows.Visibility.Hidden
+        txtActu.Visibility = Windows.Visibility.Visible
+        txtActu.DataContext = Nothing
+        Dim lesactu2 = (From act In DM.tblActualite Where act.TitreActu = (CType(lstNouvelles.SelectedItem, tblActualite).TitreActu) Select act)
+        txtActu.DataContext = CType(lesactu2.First, tblActualite)
+    End Sub
+
+    Private Sub btnX_Click(sender As Object, e As RoutedEventArgs) Handles btnX.Click
+        Me.Close()
+        Me.Finalize()
+    End Sub
+
+    Private Sub btnBack_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles btnBack.MouseDown
+        lstNouvelles.Visibility = Windows.Visibility.Visible
+        txtActu.Visibility = Windows.Visibility.Hidden
     End Sub
 End Class
