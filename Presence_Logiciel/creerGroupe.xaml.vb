23ba8964d1ef81c3bf73bcc578a089541adac285
SimonChouine <simon230293@hotmail.com>
2013-12-04 08:46:02 -0500

    Try Catch Fix

--- Presence_Logiciel/creerGroupe.xaml.vb
+++ Presence_Logiciel/creerGroupe.xaml.vb
@@ -1,27 +1,31 @@
 ﻿Public Class creerGroupe
 
     Public Forme As PresenceEntities
+    Private app As FonctionsGlobales
 
 
     Private Sub btncreergroupe_Click(sender As Object, e As RoutedEventArgs) Handles btncreergroupe.Click
         'Ajout d'un nouveau groupe
         Dim _nouveauGroupe = New tblGroupe
+        If app.verifier_int(txtidgroupe.Text) And app.verifier_int(txtnogroupe.Text) Then
+            _nouveauGroupe.IdGroupe = txtidgroupe.Text
+            _nouveauGroupe.NoGroupe = txtnogroupe.Text
 
-        _nouveauGroupe.IdGroupe = txtidgroupe.Text
-        _nouveauGroupe.NoGroupe = txtnogroupe.Text
-
-        Forme.tblGroupe.AddObject(_nouveauGroupe)
-        ' Validation de cet ajout
-        Dim _reponse = MessageBox.Show("Voulez-vous vraiment creer ce nouveau groupe", "Créer un groupe", MessageBoxButton.YesNo)
-        If _reponse = 6 Then
-            'Enregistrement du nouveau groupe
-            Try
-                Forme.SaveChanges()
-                MessageBox.Show("Un nouveau groupe a bien été enregistré", "Succès", MessageBoxButton.OK)
-            Catch ex As Exception
-            End Try
+            Forme.tblGroupe.AddObject(_nouveauGroupe)
+            ' Validation de cet ajout
+            Dim _reponse = MessageBox.Show("Voulez-vous vraiment creer ce nouveau groupe", "Créer un groupe", MessageBoxButton.YesNo)
+            If _reponse = 6 Then
+                'Enregistrement du nouveau groupe
+                Try
+                    Forme.SaveChanges()
+                    MessageBox.Show("Un nouveau groupe a bien été enregistré", "Succès", MessageBoxButton.OK)
+                Catch ex As Exception
+                End Try
+            Else
+                Return
+            End If
         Else
-            Return
+            app.changer_statut("Seuls les valeurs numériques sont acceptées comme IdGroupe ou no de groupe")
         End If
     End Sub
 
